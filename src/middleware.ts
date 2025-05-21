37a80ff24e8e3a8876dc5808283d07c9610db725
Isha Jasani <98201829+ishajasani@users.noreply.github.com>
2025-05-21 16:11:47 +0530

    Backend Setup

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { convexAuthNextjsMiddleware, createRouteMatcher , nextjsMiddlewareRedirect} from "@convex-dev/auth/nextjs/server";
+
+const isPublicPage = createRouteMatcher(["/signin"]);
+ 
+export default convexAuthNextjsMiddleware(async (request , { convexAuth }) => {
+    if(! isPublicPage(request) && !(await convexAuth.isAuthenticated())){
+        return nextjsMiddlewareRedirect(request , "/signin");
+    }
+    }
+);
+ 
+export const config = {
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
