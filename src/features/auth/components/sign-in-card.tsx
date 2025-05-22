d2c09ab1ce986e3274ea2c7f89bfa453fe5ffe7a
Isha Jasani <98201829+ishajasani@users.noreply.github.com>
2025-05-22 09:41:13 +0530

    Google & Github Authentication

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -6,14 +6,27 @@ import { FcGoogle} from "react-icons/fc";
 import { FaGithub} from "react-icons/fa";
 import { SignInFlow } from "../types";
 import { useState } from "react";
+import { useAuthActions } from "@convex-dev/auth/react";
 
 interface SignInCardProps {
     setState: (state: SignInFlow) => void;
 };
 
 export const SignInCard = ({ setState } : SignInCardProps) => {
+    const {signIn} = useAuthActions();
+
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
+    const [pending, setPending]  = useState(false);
+
+    const onProviderSignin = (value : "github" | "google") => {
+        setPending(true);
+        signIn(value)
+        .finally(()=>{
+            setPending(false);
+        }
+        )
+    }
 
     return(
         <Card className="w-full h-full p-8">
@@ -28,7 +41,7 @@ export const SignInCard = ({ setState } : SignInCardProps) => {
             <CardContent className="space-y-5 px-0 pb-0">
                 <form className="space-y-2.5">
                     <Input
-                    disabled={false}
+                    disabled={pending}
                     value={email}
                     onChange={(e)=>setEmail(e.target.value)}
                     placeholder="Email"
@@ -36,22 +49,22 @@ export const SignInCard = ({ setState } : SignInCardProps) => {
                     required
                     />
                     <Input
-                    disabled={false}
+                    disabled={pending}
                     value={password}
                     onChange={(e)=>setPassword(e.target.value)}
                     placeholder="Password"
                     type="password"
                     required
                     />
-                    <Button type="submit" className="w-full" size="lg" disabled={false}>
+                    <Button type="submit" className="w-full" size="lg" disabled={pending}>
                         Continue
                     </Button>
                 </form>
                 <Separator />
                 <div className="flex flex-col gap-y-2.5">
                     <Button 
-                    disabled={false}
-                    onClick={()=>{}}
+                    disabled={pending}
+                    onClick={()=> onProviderSignin("google")}
                     variant="outline"
                     size="lg"
                     className="w-full relative"
@@ -60,8 +73,8 @@ export const SignInCard = ({ setState } : SignInCardProps) => {
                         Continue with Google
                     </Button>
                     <Button 
-                    disabled={false}
-                    onClick={()=>{}}
+                    disabled={pending}
+                    onClick={()=>onProviderSignin("github")}
                     variant="outline"
                     size="lg"
                     className="w-full relative"
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,15 @@
-import { convexAuthNextjsMiddleware, createRouteMatcher , nextjsMiddlewareRedirect} from "@convex-dev/auth/nextjs/server";
+import { convexAuthNextjsMiddleware, createRouteMatcher , isAuthenticatedNextjs, nextjsMiddlewareRedirect} from "@convex-dev/auth/nextjs/server";
 
-const isPublicPage = createRouteMatcher(["/signin"]);
+const isPublicPage = createRouteMatcher(["/auth"]);
  
 export default convexAuthNextjsMiddleware(async (request , { convexAuth }) => {
     if(! isPublicPage(request) && !(await convexAuth.isAuthenticated())){
-        return nextjsMiddlewareRedirect(request , "/signin");
+        return nextjsMiddlewareRedirect(request , "/auth");
     }
+    if(isPublicPage(request) && (await convexAuth.isAuthenticated())){
+      return nextjsMiddlewareRedirect(request , "/");
     }
-);
+});
  
 export const config = {
   // The following matcher runs middleware on all routes
