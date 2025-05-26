e00b8b98ca8c8407ac05dba6a4a20d29b08fb9d2
Isha Jasani <98201829+ishajasani@users.noreply.github.com>
2025-05-26 21:11:10 +0530

    Workspace Header 1

---
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+
+export const WorkspaceHeader = () => {
+
+    return(
+        <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button 
+                    variant="transparent"
+                    className="font-semibold text-lg w-auto p-1.5 overflow-hidden"
+                    size="sm"
+                    >
+                        <span>{workspace?.name}</span>
+                    </Button>
+                </DropdownMenuTrigger>
+            </DropdownMenu>
+        </div>
+    )
+}
