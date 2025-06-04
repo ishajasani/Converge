14b922a220e8d58bab57ca6b9df1b210fc5bdd61
Isha Jasani <98201829+ishajasani@users.noreply.github.com>
2025-06-04 18:46:15 +0530

    feat(workspace sidebar) : Add workspace prefrence to invite via code

---
+++ src/app/workspace/[workspaceId]/invite-modal.tsx
@@ -0,0 +1,61 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { CopyIcon } from "lucide-react";
+import { toast } from "sonner";
+
+interface InviteModalProps {
+    open : boolean,
+    setOpen : (open : boolean) => void;
+    name : string;
+    joinCode : string;
+}
+
+export const InviteModal = ({
+    open , setOpen , name , joinCode
+} : InviteModalProps) => {
+    const handleCopy = () => {
+        const inviteLink = `${window.location.origin}/join/${workspaceId}`;
+
+        navigator.clipboard //native browser api
+        .writeText(inviteLink)
+        .then(() => toast.success("Invite link copied to clipboard"));
+    }
+
+    const workspaceId = useWorkspaceId();
+        
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        Invite People to {name}.
+                    </DialogTitle>
+                    <DialogDescription>
+                        Use the code below to invite people to your workspace.
+                    </DialogDescription>
+                </DialogHeader>
+                <div className="flex flex-col gap-y-4 items-center justify-center py-10">
+                    <p className="text-4xl font-bold tracking-widest uppercase">
+                        {joinCode}
+                    </p>
+                    <Button
+                    variant="ghost"
+                    size="sm"
+                    onClick={handleCopy}
+                    >
+                        Copy Link
+                        <CopyIcon className="size-4 ml-2"/>
+                    </Button>
+                </div>
+            </DialogContent>
+        </Dialog>
+    )
+}
