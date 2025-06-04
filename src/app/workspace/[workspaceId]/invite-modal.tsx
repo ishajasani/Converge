322fd78261ff90a340bb568ec9fe482b2d5ea553
Isha Jasani <98201829+ishajasani@users.noreply.github.com>
2025-06-04 19:05:15 +0530

    feat(workspace sidebar) : Add workspace invite prefrence to regenerate invite code

--- src/app/workspace/[workspaceId]/invite-modal.tsx
+++ src/app/workspace/[workspaceId]/invite-modal.tsx
@@ -7,8 +7,11 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import { useNewJoinCode } from "@/features/workspaces/api/use-new-join-code";
+import { useConfirm } from "@/hooks/use-confirm";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { CopyIcon } from "lucide-react";
+import { DialogClose } from "@radix-ui/react-dialog";
+import { CopyIcon, RefreshCcw } from "lucide-react";
 import { toast } from "sonner";
 
 interface InviteModalProps {
@@ -30,8 +33,30 @@ export const InviteModal = ({
     }
 
     const workspaceId = useWorkspaceId();
+    const [ConfirmDialog , confirm] = useConfirm(
+        "Are you sure?",
+        "This will deactivate the current invite code and generate a new one."
+    );
+
+    const { mutate , isPending } = useNewJoinCode();
+
+    const handleNewCode = async () => {
+        const ok = await confirm();
+
+        if(!ok) return;
+        mutate({workspaceId}, {
+            onSuccess : () => {
+                toast.success("Invite code regenerated");
+        },
+        onError : () =>{
+            toast.error("Failed to regenerate invite code")
+        }
+    })
+    }
         
     return (
+        <>
+        <ConfirmDialog/>
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogContent>
                 <DialogHeader>
@@ -55,7 +80,23 @@ export const InviteModal = ({
                         <CopyIcon className="size-4 ml-2"/>
                     </Button>
                 </div>
+                <div className="flex items-center justify-between w-full">
+                    <Button
+                    disabled={isPending}
+                    onClick={handleNewCode}
+                    variant="outline"
+                    >
+                        New Code
+                        <RefreshCcw className="size-4 ml-2"/>
+                    </Button>
+                    <DialogClose asChild>
+                        <Button>
+                            Close
+                        </Button>
+                    </DialogClose>
+                </div>
             </DialogContent>
         </Dialog>
+        </>
     )
 }
