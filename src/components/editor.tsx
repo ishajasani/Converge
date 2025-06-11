e808efcf65b0c130d0be9f2483c7b6db55cefb15
Isha Jasani <98201829+ishajasani@users.noreply.github.com>
2025-06-11 21:55:43 +0530

    feat(editor) : Message editor , feat(ui) : Add quill-toolbar customisation to global.css

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,11 +1,69 @@
-import Quill from "quill";
+import Quill, { type QuillOptions } from "quill";
+import {PiTextAa} from "react-icons/pi";
+import {MdSend} from "react-icons/md"
+import { useEffect, useRef } from "react";
+
+import "quill/dist/quill.snow.css";
+import { Button } from "./ui/button";
+import { ImageIcon, Smile } from "lucide-react";
 
 const Editor = () => {
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(()=>{
+        if(!containerRef.current) return;
+
+        const container = containerRef.current;
+        const editorContainer = container.appendChild(
+            container.ownerDocument.createElement("div"),
+        );
+
+        const options : QuillOptions = {
+            theme : "snow",
+        };
+
+        new Quill(editorContainer, options);
+
+        return () => {
+            if(container){
+                container.innerHTML = "";
+            }
+        };
+
+    },[]);
+
     return(
         <div className="flex flex-col">
             <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
-                <div>
-                    Editor
+                <div ref={containerRef} className="h-full ql-custom"/>
+                <div className="flex pb-2 px-2 z-[5]">
+                    <Button
+                    disabled={false}
+                    size="iconSm"
+                    variant="ghost"
+                    onClick={()=>{}}
+                    >
+                        <PiTextAa className="size-4"/>
+                    </Button>
+                    <Button
+                    disabled={false}
+                    size="iconSm"
+                    variant="ghost"
+                    onClick={()=>{}}
+                    >
+                        <Smile className="size-4"/>
+                    </Button>
+                    <Button
+                    disabled={false}
+                    size="iconSm"
+                    variant="ghost"
+                    onClick={()=>{}}
+                    >
+                        <ImageIcon className="size-4"/>
+                    </Button>
+                    <Button>
+                        <MdSend/>
+                    </Button>
                 </div>
             </div>
         </div>
