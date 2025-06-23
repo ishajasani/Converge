cdf8db5ea1a87cd675b1aaff8da65109dc1ca819
Isha Jasani <98201829+ishajasani@users.noreply.github.com>
2025-06-23 18:46:18 +0530

    feat(messages) : Add message toolbar

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -5,6 +5,7 @@ import { format, isToday, isYesterday } from "date-fns";
 import { Hint } from "./hint";
 import { Avatar , AvatarImage , AvatarFallback } from "./ui/avatar";
 import { Thumbnail } from "./thumbnail";
+import { Toolbar } from "./toolbar";
 
 const Renderer = dynamic(()=> import("@/components/renderer") , {ssr : false});
 
@@ -108,6 +109,17 @@ export const Message = ({
             ) : null}
             </div>
             </div>
+            {!isEditing && (
+                <Toolbar
+                isAuthor = {isAuthor}
+                isPending = {false}
+                handleEdit = {()=>setEditingId(id)}
+                handleThread = {()=>{}}
+                handleDelete = {()=>{}}
+                handleReaction = {()=>{}}
+                hideThreadButton = {hideThreadButton}
+                />
+            )}
         </div>
      );
 }
